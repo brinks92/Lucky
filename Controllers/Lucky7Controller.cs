da91d6156ba9bc243f34a66f955b07281b5e1c58
Roberto Ramirez <robertramirez658@outlook.com>
2018-01-16 14:39:02 -0800

    This is new one

---
+++ Controllers/Lucky7Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class Lucky7Controller : Controller
+    {
+        // GET: Lucky7
+        public ActionResult Index(int luck = 7)
+        {
+            Random rnd = new Random();
+            int lucky = luck;
+            int a = rnd.Next(0, 10);
+            int b = rnd.Next(0, 10);
+            int c = rnd.Next(0, 10);
+
+            var display = "none";
+
+
+           if (a == lucky || b == lucky || c == lucky)
+            {
+                 display = "block";
+            }
+
+            
+            ViewBag.number = lucky;
+            ViewBag.first = a;
+            ViewBag.sec = b;
+            ViewBag.third = c;
+            ViewBag.display = display;
+            return View();
+        }
+    }
+}
